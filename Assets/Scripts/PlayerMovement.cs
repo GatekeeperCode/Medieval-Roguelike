36060536e4975c1c957b0250d9c64dca1140443c
GatekeeperCode <mikegburiok@gmail.com>
2024-06-03 18:29:05 -0400

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _speed;
+    public float _physStren;
+    public float _rangStren;
+    public float _magStren;
+    public float _defense;
+    public int _gold;
+
+    Rigidbody2D _rbody;
+
+
+    void Start()
+    {
+        _rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        _rbody.velocity = new Vector2(x, y) * _speed;
+    }
+}
