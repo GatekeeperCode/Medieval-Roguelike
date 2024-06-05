02d42ee66765bd94c7128b0f5b7c9d37f48809c6
GatekeeperCode <mikegburiok@gmail.com>
2024-06-05 16:59:41 -0400

    Room Spawning Termination

--- Assets/Scripts/DoorwayTrigger.cs
+++ Assets/Scripts/DoorwayTrigger.cs
@@ -10,9 +10,29 @@ public class DoorwayTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 faceDirection;
+
+        if(_dir == Vector2.left * 11.5f)
+        {
+            faceDirection = Vector2.left;
+        }
+        else if(_dir == Vector2.right * 11.5f)
+        {
+            faceDirection = Vector2.right;
+        }
+        else if(_dir == Vector2.up * 4.5f)
+        {
+            faceDirection = Vector2.up;
+        }
+        else
+        {
+            faceDirection = Vector2.down;
+        }
+
+
         if(!isInContact(_dir))
         {
-            GameObject.FindGameObjectWithTag("SceneManager").GetComponent<RoomSpawnScript>().spawnRoom(new Vector2(transform.position.x + _dir.x, transform.position.y + _dir.y));
+            GameObject.FindGameObjectWithTag("SceneManager").GetComponent<RoomSpawnScript>().spawnRoom(new Vector2(transform.position.x + _dir.x, transform.position.y + _dir.y), faceDirection);
         }
     }
 
--- Assets/Scripts/RoomSpawnScript.cs
+++ Assets/Scripts/RoomSpawnScript.cs
@@ -5,10 +5,46 @@ using UnityEngine;
 public class RoomSpawnScript : MonoBehaviour
 {
     public GameObject _basicRoom;
+    public GameObject _sideRoom;
+    public GameObject _topRoom;
 
-    public void spawnRoom(Vector2 centerPos)
+    public void spawnRoom(Vector2 centerPos, Vector2 dir)
     {
         print("Room Spawned");
-        Instantiate(_basicRoom, centerPos, Quaternion.identity);
+        int rand = Random.Range(0, 5);
+
+        if (rand == 0)
+        {
+            Instantiate(_basicRoom, centerPos, Quaternion.identity);
+        }
+        else
+        {
+            if(dir == Vector2.down)
+            {
+                Instantiate(_topRoom, centerPos, Quaternion.identity);
+            }
+            else if(dir == Vector2.up)
+            {
+                GameObject room = Instantiate(_topRoom, centerPos, Quaternion.identity);
+                room.transform.eulerAngles = new Vector3(
+                    room.transform.eulerAngles.x,
+                    room.transform.eulerAngles.y,
+                    room.transform.eulerAngles.z + 180
+                );
+            }
+            else if(dir == Vector2.left)
+            {
+                Instantiate(_sideRoom, centerPos, Quaternion.identity);
+            }
+            else
+            {
+                GameObject room = Instantiate(_sideRoom, centerPos, Quaternion.identity);
+                room.transform.eulerAngles = new Vector3(
+                    room.transform.eulerAngles.x,
+                    room.transform.eulerAngles.y,
+                    room.transform.eulerAngles.z + 180
+                );
+            }
+        }
     }
 }
