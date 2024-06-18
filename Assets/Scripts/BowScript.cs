e6ec41e6f7471401ba62076143fb1a486df2547f
GatekeeperCode <mikegburiok@gmail.com>
2024-06-18 15:57:55 -0400

    Bow Adding

---
+++ Assets/Scripts/BowScript.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour
+{
+    public GameObject bowGO;
+    public GameObject projectile;
+
+    public float _chargeFactor;
+
+    Transform bow;
+    Camera mainCamera;
+    GameObject bowObject;
+    float _charge;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bow = bowGO.transform;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        bowObject = bowGO.transform.GetChild(2).gameObject;
+        sr = bowObject.GetComponent<SpriteRenderer>();
+        bowObject.SetActive(true);
+        _charge = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PointToMouse(bow);
+
+        if(Input.GetMouseButton(0))
+        {
+            if(_charge<10)
+            {
+                _charge += Time.deltaTime * _chargeFactor;
+
+                if(_charge>=10)
+                {
+                    _charge = 10;
+                    sr.color = Color.black;
+                }
+                else if(_charge>7)
+                {
+                    sr.color = Color.green;
+                }
+                else if(_charge>4)
+                {
+                    sr.color = Color.yellow;
+                }
+                else
+                {
+                    sr.color = Color.red;
+                }
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            print(bowObject.transform.rotation);
+            GameObject arrow = Instantiate(projectile, bowObject.transform.GetChild(0).transform.position, Quaternion.identity);
+            arrow.transform.rotation = bowObject.transform.rotation * Quaternion.Euler(0,0,90);
+            arrow.GetComponent<Rigidbody2D>().AddForce(arrow.transform.up * -10 * _charge);
+            _charge = 0;
+            sr.color = Color.red;
+        }
+    }
+
+    //https://www.antoniovalentini.com/rotate-an-object-towards-the-mouse-position-2d/
+    private void PointToMouse(Transform player)
+    {
+        var mouseScreenPos = Input.mousePosition;
+        var startingScreenPos = mainCamera.WorldToScreenPoint(player.position);
+        mouseScreenPos.x -= startingScreenPos.x;
+        mouseScreenPos.y -= startingScreenPos.y;
+        var angle = Mathf.Atan2(mouseScreenPos.y, mouseScreenPos.x) * Mathf.Rad2Deg;
+        player.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
