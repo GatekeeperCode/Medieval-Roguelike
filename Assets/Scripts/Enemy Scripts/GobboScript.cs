4491fbfd368a613020b3b2f95a5e5c27caaebf84
GatekeeperCode <mikegburiok@gmail.com>
2024-07-04 15:45:45 -0400

    Gobbo health and hitstun added

--- Assets/Scripts/Enemy Scripts/GobboScript.cs
+++ Assets/Scripts/Enemy Scripts/GobboScript.cs
@@ -7,16 +7,56 @@ public class GobboScript : MonoBehaviour
     GameObject player;
     public float health;
     public float speed;
+    bool hitStun = false;
+    Color _c;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        _c = GetComponent<SpriteRenderer>().color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        if(!hitStun)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+
+        if (health<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Floor")
+        {
+            if (collision.gameObject.tag == "Sword" || collision.gameObject.tag == "Spear")
+            {
+                health -= collision.gameObject.GetComponent<MeleeDmgScript>().damage;
+                GetComponent<SpriteRenderer>().color = Color.red;
+                hitStun = true;
+                StartCoroutine(hitReg());
+            }
+
+            if (collision.gameObject.tag == "Arrow")
+            {
+                health -= collision.gameObject.GetComponent<ArrowScript>().damage;
+                GetComponent<SpriteRenderer>().color = Color.red;
+                hitStun = true;
+                StartCoroutine(hitReg());
+            }
+        }
+    }
+
+    private IEnumerator hitReg()
+    {
+        yield return new WaitForSeconds(.25f);
+        GetComponent<SpriteRenderer>().color = _c;
+        hitStun = false;
     }
 }
