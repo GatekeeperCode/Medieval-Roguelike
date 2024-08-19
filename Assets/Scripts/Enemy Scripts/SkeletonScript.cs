1a9abb384ce5e79fe6acc723127e1cd2d6666f40
GatekeeperCode <mikegburiok@gmail.com>
2024-08-19 14:47:41 -0400

    Skeleton Adjustments + BulletHell Room

--- Assets/Scripts/Enemy Scripts/SkeletonScript.cs
+++ Assets/Scripts/Enemy Scripts/SkeletonScript.cs
@@ -7,6 +7,7 @@ public class SkeletonScript : EnemyBase
     public GameObject arrowPrefab;
     public float fireRate;
     public float skeleDmg;
+    public float range;
 
     bool hitStun = false;
 
@@ -24,11 +25,15 @@ public class SkeletonScript : EnemyBase
     {
         if(roomVars.playerPresent)
         {
-            Quaternion rotation = Quaternion.LookRotation(
-                player.transform.position - transform.position,
-                transform.TransformDirection(Vector3.up)
-                );
-            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+            Vector3 targ = player.transform.position;
+            targ.z = 0f;
+
+            Vector3 objectPos = transform.position;
+            targ.x = targ.x - objectPos.x;
+            targ.y = targ.y - objectPos.y;
+
+            float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
 
         if (health <= 0)
@@ -41,13 +46,13 @@ public class SkeletonScript : EnemyBase
     {
         while(true)
         {
-            yield return new WaitForSeconds(fireRate);
+            yield return new WaitForSeconds(Random.Range(fireRate-2f, fireRate+2f));
 
-            if (roomVars.playerPresent && !hitStun)
+            if (roomVars.playerPresent && !hitStun && Vector2.Distance(transform.position, player.transform.position) < range)
             {
                 GameObject arrow = Instantiate(arrowPrefab, transform.GetChild(0).transform.position, Quaternion.identity);
                 arrow.transform.rotation = transform.rotation * Quaternion.Euler(0, 0, 90);
-                arrow.GetComponent<Rigidbody2D>().AddForce(arrow.transform.up * 50);
+                arrow.GetComponent<Rigidbody2D>().AddForce(arrow.transform.up * -50);
                 arrow.GetComponent<ArrowScript>().damage = skeleDmg;
             }
         }
