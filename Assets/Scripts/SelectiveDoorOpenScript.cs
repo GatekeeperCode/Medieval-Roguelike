4e75464877857ba9587583bc7875decd1cd3a422
GatekeeperCode <mikegburiok@gmail.com>
2024-09-17 15:05:50 -0400

    Dungeon, Top Clone, Chapel

--- Assets/Scripts/SelectiveDoorOpenScript.cs
+++ Assets/Scripts/SelectiveDoorOpenScript.cs
@@ -8,12 +8,6 @@ public class SelectiveDoorOpenScript : MonoBehaviour
     public GameObject[] walls;
     public bool hasOpenedDoor;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
