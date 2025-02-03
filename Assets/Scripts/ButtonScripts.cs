25b700a21561dc7b01874b6f5879b73fd448bfeb
GatekeeperCode <mikegburiok@gmail.com>
2025-02-03 17:19:35 -0500

    Main Menu + Menu Loops

--- Assets/Scripts/ButtonScripts.cs
+++ Assets/Scripts/ButtonScripts.cs
@@ -1,34 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonScripts : MonoBehaviour
 {
-    PlayerMovement pmove;
+    public GameObject mainScreen;
+    public GameObject optionsScren;
+    GameObject pmove;
 
     // Start is called before the first frame update
     void Start()
     {
-        pmove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        pmove = GameObject.FindGameObjectWithTag("Player");
     }
 
     
     public void onMenuButtonPressed()
     {
         //change scene to main menu
-        print("Menu scene transition not enabled yet.");
+        //print("Menu scene transition not enabled yet.");
+        SceneManager.LoadScene(0);
     }
 
     public void onOptionPressed()
     {
-        //Open Options Screen
-        print("Options Menu Not Enabled Yet");
+        mainScreen.SetActive(false);
+        optionsScren.SetActive(true);
     }
 
     public void onResumePressed()
     {
         Time.timeScale = 1.0f;
-        pmove.pauseMenu.SetActive(false);
-        pmove.paused = false;
+        mainScreen.SetActive(false);
+        pmove.GetComponent<PlayerMovement>().paused = false;
+    }
+
+    public void pauseBackButton()
+    {
+        mainScreen.SetActive(true);
+        optionsScren.SetActive(false);
+    }
+
+    public void onStartPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitButtonPressed()
+    {
+        Application.Quit();
     }
 }
