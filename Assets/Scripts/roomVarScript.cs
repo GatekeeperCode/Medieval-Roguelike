c4d646f530585044bff458bb34106982e9a0356f
GatekeeperCode <mikegburiok@gmail.com>
2025-03-14 17:06:48 -0400

    Monsters Start Unspawned Change

--- Assets/Scripts/roomVarScript.cs
+++ Assets/Scripts/roomVarScript.cs
@@ -6,4 +6,24 @@ public class roomVarScript : MonoBehaviour
 {
     public bool isFlipped = false;
     public bool playerPresent = false;
+
+    public GameObject[] monsters;
+
+    bool check = false;
+
+    private void Update()
+    {
+        if(playerPresent && !check)
+        {
+            if(monsters.Length != 0)
+            {
+                for(int i=0; i<monsters.Length; i++)
+                {
+                    monsters[i].SetActive(true);
+                }
+            }
+
+            check = true;
+        }
+    }
 }
