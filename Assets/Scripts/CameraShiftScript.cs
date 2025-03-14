8d74614f01fc53f2dde1d3b224f0f76e9cc8399c
GatekeeperCode <mikegburiok@gmail.com>
2025-03-14 17:03:19 -0400

    Doorway Trigger Deletion Check Add

--- Assets/Scripts/CameraShiftScript.cs
+++ Assets/Scripts/CameraShiftScript.cs
@@ -24,4 +24,9 @@ public class CameraShiftScript : MonoBehaviour
             camObject.transform.position = new Vector3(transform.position.x + _dir.x, transform.position.y + _dir.y, camObject.transform.position.z);
         }
     }
+
+    private void OnDestroy()
+    {
+        print("Ow, I was destroyed for some reason");
+    }
 }
